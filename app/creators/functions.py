8a1008bbeca718d5c61c3cadc31c47afb07904db
Laura Siepman <56088732+lsiepman@users.noreply.github.com>
2025-08-23 21:36:13 +0200

    working on the add creator form

--- app/creators/functions.py
+++ app/creators/functions.py
@@ -1,9 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, DateField, BooleanField, SubmitField
 from wtforms.validators import InputRequired
 
 
 class CreatorForm(FlaskForm):
     first_name = StringField("First name", validators=[InputRequired()])
-    family_name_prep = StringField("Family name preposition")
+    family_name_prep = StringField("Infix")
     last_name = StringField("Last name", validators=[InputRequired()])
+    nationality = StringField("Nationality")
+    dob = DateField("Date of birth")
+    pseudonym = BooleanField("Name is pseudonym")
+    real_first_name = StringField("First name")
+    real_family_name_prep = StringField("Infix")
+    real_last_name = StringField("Last name")
+    submit = SubmitField("Submit")
